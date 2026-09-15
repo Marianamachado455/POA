3881169e3aabfb1d1631a84c6eb8cca4d1e797e7
Mariana Machado <mariana.25104708360006@faeterj-rio.edu.br>
2026-09-15 12:36:25 -0300

    Corrigindo arq bat

--- AplicacaoWebService/atualizar.bat
+++ AplicacaoWebService/atualizar.bat
@@ -2,21 +2,21 @@
 
 echo Parando Tomcat...
 call D:\Programas\apache-tomcat-9.0.121\bin\shutdown.bat
-
 timeout /t 2 /nobreak
 
 echo Removendo versao antiga...
-rmdir /S /Q D:\Programas\apache-tomcat-9.0.121\webapps\ProjetoNovoWeb-1.0.0
+rmdir /S /Q D:\Programas\apache-tomcat-9.0.121\webapps\AplicacaoWebService
+del /Q D:\Programas\apache-tomcat-9.0.121\webapps\AplicacaoWebService.war
 
 echo Gerando projeto...
 call mvn clean package
 
 echo Copiando projeto...
-copy /Y target\ProjetoNovoWeb-1.0.0.war D:\Programas\apache-tomcat-9.0.121\webapps\ProjetoNovoWeb-1.0.0.war
+copy /Y target\AplicacaoWebService.war D:\Programas\apache-tomcat-9.0.121\webapps\AplicacaoWebService.war
 
 echo Iniciando Tomcat...
 call D:\Programas\apache-tomcat-9.0.121\bin\startup.bat
-
 echo.
 echo Projeto atualizado!
+
 pause
