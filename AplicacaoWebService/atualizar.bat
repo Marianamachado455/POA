933c78e67bd320f49f1a4bef9fab16341298d782
Mariana Machado <mariana.25104708360006@faeterj-rio.edu.br>
2026-09-15 12:59:00 -0300

    adicionando header basica

--- AplicacaoWebService/atualizar.bat
+++ AplicacaoWebService/atualizar.bat
@@ -1,22 +1,22 @@
 @echo off
 
 echo Parando Tomcat...
-call D:\Programas\apache-tomcat-9.0.121\bin\shutdown.bat
-timeout /t 2 /nobreak
+call "D:\Programas\apache-tomcat-9.0.121\bin\shutdown.bat"
 
-echo Removendo versao antiga...
-rmdir /S /Q D:\Programas\apache-tomcat-9.0.121\webapps\AplicacaoWebService
-del /Q D:\Programas\apache-tomcat-9.0.121\webapps\AplicacaoWebService.war
+timeout /t 3 /nobreak >nul
 
-echo Gerando projeto...
+echo Criando projeto com Maven...
 call mvn clean package
 
-echo Copiando projeto...
-copy /Y target\AplicacaoWebService.war D:\Programas\apache-tomcat-9.0.121\webapps\AplicacaoWebService.war
+echo Apagando versao antiga...
+del /f /q "D:\Programas\apache-tomcat-9.0.121\webapps\AplicacaoWebService.war"
+rmdir /s /q "D:\Programas\apache-tomcat-9.0.121\webapps\AplicacaoWebService"
+
+echo Copiando nova versao...
+copy /Y "target\AplicacaoWebService.war" "D:\Programas\apache-tomcat-9.0.121\webapps\"
 
 echo Iniciando Tomcat...
-call D:\Programas\apache-tomcat-9.0.121\bin\startup.bat
-echo.
-echo Projeto atualizado!
+call "D:\Programas\apache-tomcat-9.0.121\bin\startup.bat"
 
+echo Deploy concluido!
 pause
--- AplicacaoWebService/src/main/webapp/index.jsp
+++ AplicacaoWebService/src/main/webapp/index.jsp
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PreviClima</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css?v=2">
 </head>
+
 <body>
-    <h2>PreviClima</h2>
+
+    <header>
+        PreviClima
+    </header>
+
+    <h2>Bem-vindo ao PreviClima!</h2>
+    <div class="cardPlaceholder"></div>
 </body>
 </html>
